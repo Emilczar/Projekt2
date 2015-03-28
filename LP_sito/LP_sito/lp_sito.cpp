bac8861dda990cc52ca5ff0b0a680a37f6cfb0eb
Emilczar <emilczar@gmail.com>
2015-03-28 18:41:48 +0100

    dodałem komentarze i działa chyba prawidłowo:)

--- LP_sito/LP_sito/lp_sito.cpp
+++ LP_sito/LP_sito/lp_sito.cpp
@@ -3,44 +3,42 @@
 #include<omp.h>
 #include<stdlib.h>
 
-#define N 10000000
-#define S sqrt(N)
+#define N 1000000 // zakres
+#define S 1000 // z braku pomys³u aby dzia³a³o sprt(N) wpisa³em recznie
 #define M N/10
 
-int main(int argc, char** argv){
-	
-	
-	int a[S + 1];
-	long int pierwsza[M];
+int main(){
+	long int a[S + 1]; // tablica pomocnicza
+	long int pierwsza[M]; // liczby pierwsze w przedziale 2..N
 	long int i, k,liczba,reszta;
-	long int lpodz;
-	long int llpier = 0;
-	double czas;
+	long int lpodz; // liczba podzielnikow
+	long int llpier = 0; // liczba  liczb pieewszych
+	double czas;// zmiena do mierzenia czasu
 
 	czas = omp_get_wtime();
-#pragma omp parallel for default(none) shared(a)
-	for (i = 2; i <= S; i++)
-	for (i = 2; i <= S; i++)
+	printf("start \n");
+#pragma omp parallel for shared(a)
+	
+	for (i =2; i <= S; i++) a[i] = 1; // inicjowanie
+	for (i =2; i <= S; i++)
 	if (a[i] == 1){
-		pierwsza[llpier++] = i;
-		for (k = i + i; k <= S; k += i) a[k] = 0;
+	pierwsza[llpier++] = i; //zapamietanie podzielnika
+	for (k = i + i; k <= S; k += i) a[k] = 0;
 	}
 
-	lpodz = llpier;
-#pragma omp parallel for default(nome) private(k,reszta) shared(llpier,lpodz,pierwsza)
-	for (liczba = S + 1; liczba <= N;liczba++){
-		for (k = 0; k < lpodz; k++){
-			reszta = (liczba %pierwsza[k]);
+	lpodz = llpier; //zapamietanie liczby podzielnikow
+#pragma omp parallel for  private(k, reszta) shared(llpier, lpodz, pierwsza)
+	for (liczba = S + 1; liczba <= N; liczba++){
+		for (k = 0; k < lpodz; k++) {
+			reszta = (liczba % pierwsza[k]);
 			if (reszta == 0) break;
 		}
 		if (reszta != 0){
-#pragma omp critical
-			pierwsza[llpier++] = liczba;
+			#pragma omp critical
+			pierwsza[llpier++] = liczba; // zapamietanie liczby pierwszej
 		}
 	}
-	
 	czas = omp_get_wtime()- czas;
 	printf("czas: %f sek \n", czas);
-
 	return 0;
 }
